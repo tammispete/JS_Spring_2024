9f34f558244b5db0965313e0258820d597cb59ca
tammispete <tamminen.petteri@gmail.com>
2024-03-04 01:26:31 +0200

    finished assigment

---
+++ lecture_ex/myJavascript.js
@@ -0,0 +1,13 @@
+alert("Hello world!");
+
+function myFunction(a,b){
+    return a+b;
+}
+
+function myFirstFunction(number){
+    for(var i=0; i< number;i++){
+        console.log(i);
+    }
+
+return 10;
+}
